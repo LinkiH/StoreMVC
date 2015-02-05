2e04037a09904e1c65d63c9a92f722fffbc97aa0
LinkiH <a.kostyrin@mail.ru>
2015-02-05 22:20:15 +0200

    First commit

---
+++ StoreMVC.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreMVC.Domain.Entities;
+
+namespace StoreMVC.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> Products { get; set; }
+    }
+}
