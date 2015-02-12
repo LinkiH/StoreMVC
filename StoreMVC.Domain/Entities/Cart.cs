5fc1f55707e7f9303ac0674221fe13bae370484c
LinkiH <a.kostyrin@mail.ru>
2015-02-12 17:55:45 +0200

    UnitTests

---
+++ StoreMVC.Domain/Entities/Cart.cs
@@ -0,0 +1,46 @@
+﻿
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreMVC.Domain.Entities
+{
+    public class Cart : Entity
+    {
+        private List<CartLine>  _lines = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = _lines.FirstOrDefault(p => p.Product.Equals(product));
+            if (line == null)
+            {
+                _lines.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+        }
+
+        public void RemoveLive(Product product)
+        {
+            _lines.RemoveAll(l => l.Product.Equals(product));
+        }
+        public decimal ComputeTotalValue()
+        {
+            return _lines.Sum(e => e.Product.Price*e.Quantity);
+        }
+
+        public void Clear()
+        {
+            _lines.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return _lines; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ StoreMVC.Tests/UnitTests/CartTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreMVC.Domain.Entities;
+
+namespace StoreMVC.Tests.UnitTests
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            Product p1 = new Product {Name = "P1"};
+            Product p2 = new Product { Name = "P2" };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Product, p1);
+            Assert.AreEqual(results[1].Product, p2);
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            Product p1 = new Product { Name = "P1" };
+            Product p2 = new Product { Name = "P2" };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines.OrderBy(c => c.Product.Id).ToArray();
+
+            Assert.AreEqual(results.Length, 2);
+            Assert.AreEqual(results[0].Quantity, 11);
+            Assert.AreEqual(results[1].Quantity, 1);
+        }
+
+        [TestMethod]
+        public void Can_Remote_Line()
+        {
+            Product p1 = new Product { Name = "P1" };
+            Product p2 = new Product { Name = "P2" };
+            Product p3 = new Product { Name = "P3" };
+
+            Cart target = new Cart();
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            target.RemoveLive(p2);
+
+            Assert.AreEqual(target.Lines.Where(c => c.Product.Equals(p2)).Count(), 0);
+            Assert.AreEqual(target.Lines, 0);
+        }
+    }
+}
